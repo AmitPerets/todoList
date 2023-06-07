16a3469b70e0f3651e9e9dc25de5a501cd1c8949
Amit Perets <106886074+AmitPerets@users.noreply.github.com>
2023-06-07 22:03:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+
+app.get('/', (req, res) => {
+    res.render('todo.ejs');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(3000, () => console.log("Server Up and running"));
