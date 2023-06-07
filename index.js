eaf15e5cee080472b0a78ea6a49c97ec3da7ddb5
Amit Perets <106886074+AmitPerets@users.noreply.github.com>
2023-06-07 23:24:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
-
 dotenv.config();
+
+const mongoose = require("mongoose");
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => console.log("Server Up and running"));
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.set("view engine", "ejs");
 
@@ -20,4 +30,3 @@ app.post('/', (req, res) => {
     console.log(req.body);
 });
 
-app.listen(3000, () => console.log("Server Up and running"));
