0b1bdc0d46728a35564d5b9b0a7d43c44b351fb3
Amit Perets <106886074+AmitPerets@users.noreply.github.com>
2023-06-07 23:55:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,42 @@ app.post('/',async (req, res) => {
     res.redirect("/");
     }
     });
+
+
+ app.route("/edit/:id")
+  .get(async (req, res) => {
+    try {
+      const id = req.params.id;
+      const tasks = await TodoTask.find({});
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    } catch (err) {
+      console.error(err);
+      // Handle the error
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const id = req.params.id;
+      await TodoTask.findByIdAndUpdate(id, { content: req.body.content });
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      // Handle the error
+      res.status(500).send(err);
+    }
+  });
+
+  app.route("/remove/:id").get(async (req, res) => {
+    try {
+      const id = req.params.id;
+      await TodoTask.findByIdAndRemove(id);
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      // Handle the error
+      res.status(500).send(err);
+    }
+  });
+  
+
+  
